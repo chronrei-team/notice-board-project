f7dd7b43739a0d27440f2bac5e3fdb87be3b4fd5
Jung Sumin <drt00784@naver.com>
2025-05-30 22:45:03 +0900

    Update: 우측 상단 '프로필 설정' 버튼으로 정보 수정 페이지에 이동하도록 변경한다

--- src/main/java/notice/project/filter/TokenFilter.java
+++ src/main/java/notice/project/filter/TokenFilter.java
@@ -2,13 +2,12 @@ package notice.project.filter;
 
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 
 import java.io.IOException;
 
 @WebFilter("/*")
-public class AuthorizationFilter implements Filter {
+public class TokenFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
