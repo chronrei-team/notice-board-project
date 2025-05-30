36c71c3a82cfa381e3a8e76d5a951ebadae94914
Jung Sumin <drt00784@naver.com>
2025-05-30 18:12:53 +0900

    Feat: 로그아웃 기능을 추가한다다

---
+++ src/main/java/notice/project/auth/controller/LogoutController.java
@@ -0,0 +1,19 @@
+package notice.project.auth.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/auth/logout")
+public class LogoutController extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getSession().invalidate();
+        request.setAttribute("token", null);
+        request.getRequestDispatcher("/").forward(request, response);
+    }
+}
