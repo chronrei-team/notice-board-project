1a1f3887f75fd52971979a95e01c262365d66365
Jung Sumin <drt00784@naver.com>
2025-06-02 21:32:31 +0900

    Update: 로그아웃 기능을 로그인 상태에서만 가능하게 수정한다

--- src/main/java/notice/project/auth/controller/LogoutController.java
+++ src/main/java/notice/project/auth/controller/LogoutController.java
@@ -5,12 +5,15 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import notice.project.core.AuthBaseServlet;
+import notice.project.core.Authorization;
 
 import java.io.IOException;
 
 @WebServlet("/auth/logout")
-public class LogoutController extends HttpServlet {
+public class LogoutController extends AuthBaseServlet {
     @Override
+    @Authorization
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getSession().invalidate();
         request.setAttribute("token", null);
